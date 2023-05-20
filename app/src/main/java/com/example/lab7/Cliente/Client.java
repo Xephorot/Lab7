25a2776dce16903a818f663b3f441c135c8c71a2
Xephorot <escalantesergio821@gmail.com>
2023-05-20 12:39:40 -0400

    Server y Client Creados

---
+++ app/src/main/java/com/example/lab7/Cliente/Client.java
@@ -0,0 +1,45 @@
+package com.example.lab7.Cliente;
+import java.net.*;
+import java.io.*;
+
+public class Client {
+    private Socket socket = null;
+    private DataOutputStream out = null;
+    private DataInputStream in = null;
+
+    public Client(String address, int port) {
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            in = new DataInputStream(System.in);
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch(UnknownHostException u) {
+            System.out.println(u);
+        } catch(IOException i) {
+            System.out.println(i);
+        }
+
+        String line = "";
+        while (!line.equals("End")) {
+            try {
+                line = in.readLine();
+                out.writeUTF(line);
+            } catch(IOException i) {
+                System.out.println(i);
+            }
+        }
+
+        try {
+            in.close();
+            out.close();
+            socket.close();
+        } catch(IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]) {
+        Client client = new Client("127.0.0.1", 5000);
+    }
+}
